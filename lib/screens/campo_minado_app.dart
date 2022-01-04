9fb03437b1c11a4b25d498e2f5e23ff5974bfc21
Daniel Ernany <ddernany@gmail.com>
2022-01-04 14:04:11 -0300

    feat: criei ResultadoWidget para mostrar o resultado da partida na AppBar

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import '../components/resultado_widget.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({Key? key}) : super(key: key);
+
+  _reiniciar() {
+    print('reiniciar ...');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Campo minado'),
+        appBar: ResultadoWidget(
+          venceu: null,
+          onReiniciar: _reiniciar,
         ),
         body: Container(
           child: Text('Tabuleiro'),
